86ffb1e3e20885b9c32574026048a7ef2f33af10
ISMAIL ABDULLAHI YUSUF <ismailawmohamed1@gmail.com>
2024-11-01 03:21:31 +0800

    done Lab3

---
+++ index.js
@@ -0,0 +1,3 @@
+let button = document.getElementsByTagName('button')[3];
+button.addEventListener('click', function() {alert('in 2023 Me and 12 Indonosian Students from UTHM we visited Javanese people and see Their Culture in Parit Mugis Johor')});
+
